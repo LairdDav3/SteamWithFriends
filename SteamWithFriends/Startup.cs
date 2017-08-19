1a2c9f7b19263b30d6b2c7bc89c7135eaa65fc2b
Dav3 <khaoscreates@gmail.com>
2017-08-19 13:12:05 +0100

    Create Steam API Service

--- SteamWithFriends/Startup.cs
+++ SteamWithFriends/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SteamWithFriends.Services;
 
 namespace SteamWithFriends
 {
@@ -23,6 +24,8 @@ namespace SteamWithFriends
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ISteamApiService, SteamApiService>();
+
             services.AddMvc();
         }
 
